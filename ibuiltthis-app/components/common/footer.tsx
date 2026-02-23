0e9e1853cd8ef9e68bbd95b92ca283401d047d33
Ravi Ladumor <ladumorravi1@gmail.com>
2026-02-23 19:57:52 +0530

    Remove ibuiltthis-app folder

--- ibuiltthis-app/components/common/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="boder-t bg-muted/20 py-12">
-      <div className="wrapper">
-        IBhuiltThis Inc. All rights reserved.
-      </div>
-    </footer>
-  );
-}
